57e924aec607956d1362add8841c9337ee828175
한지원 <gomdeschool@naver.com>
2024-08-20 08:56:47 +0900

    todo-list

--- todo.js
+++ todo.js
@@ -34,85 +34,96 @@ const deleteTodo = (todoId) => {
 
 
 const paintTodos = () => {
-    todoListEl.innerHTML = ''; //todoListElem 요소 안의 HTML 초기화
-	  const allList = getAllTodos() // todos 배열 가져오기
+  //todoListElem 요소 안의 HTML 초기화
+  todoListEl.innerHTML = ''; 
+  // todos 배열 가져오기
+	const allList = getAllTodos() 
 
-    allList.forEach(todo => { 
-        const todoItemEl = document.createElement("li");
-        todoItemEl.classList.add("todo-item");
+  allList.forEach(todo => { 
+    const todoItemEl = document.createElement("li");
+    todoItemEl.classList.add("todo-item");
 
-        todoItemEl.setAttribute('data-id', todo.id );
+    todoItemEl.setAttribute('data-id', todo.id );
 
-        const checkboxEl = document.createElement('button');
-        checkboxEl.classList.add('checkbox');
+    // 체크박스
+    const checkboxEl = document.createElement('button');
+    checkboxEl.classList.add('checkbox');
 
-        const todoEl = document.createElement('div');
-        todoEl.classList.add('todo');
-        todoEl.innerText = todo.content;
+    const todoEl = document.createElement('div');
+    todoEl.classList.add('todo');
+    todoEl.innerText = todo.content;
 
-        const deleteBtn = document.createElement("button");
-        deleteBtn.classList.add('delBtn');
-        deleteBtn.addEventListener('click', () =>  deleteTodo(todo.id))
-        deleteBtn.innerHTML = `<span class="material-symbols-outlined">
-          delete</span>`;
+    // 체크박스 클릭시 완료 표시
+    checkboxEl.addEventListener('click', () => { 
+      todoItemEl.classList.toggle('complete');
+    });
 
-        const updateBtn = document.createElement("button");
-        updateBtn.classList.add("updateBtn");
-        updateBtn.innerHTML = `<span class="material-symbols-outlined">
-          edit</span>`;
-        updateBtn.addEventListener("click", (e) => {
-          changeText.classList.remove("off");
-          changeTextOk.classList.remove("off");
-        })
+    // delete 버튼
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add('delBtn');
+    deleteBtn.addEventListener('click', () =>  deleteTodo(todo.id))
+    deleteBtn.innerHTML = `<span class="material-symbols-outlined">
+      delete</span>`;
 
-        const changeText = document.createElement("input");
-        changeText.classList.add("change-text");
-        changeText.classList.add("off");
-
-        // 확인버튼 누르면 다시 off 
-        const changeTextOk = document.createElement("button");
-        changeTextOk.classList.add("change-text_ok");
-        changeTextOk.classList.add("off");
-        changeTextOk.innerHTML= "확인"
-        changeTextOk.addEventListener("click", (e) => {
-          if(todoEl.value === changeText.value || changeText.value === "") {
-            changeText.classList.add("off");
-            changeTextOk.classList.add("off");
-          }else {
-            todoEl.value = changeText.value;
-            changeText.classList.add("off");
-            changeTextOk.classList.add("off");
-          }
-        })
-
-        checkboxEl.addEventListener('click', () => { // 체크박스 클릭시 완료 표시
-          todoItemEl.classList.toggle('complete');
-        });
-
-        todoItemEl.appendChild(checkboxEl);
-        todoItemEl.appendChild(todoEl);
-        todoItemEl.appendChild(deleteBtn);
-        todoItemEl.appendChild(updateBtn);
-        todoItemEl.appendChild(changeText);
-        todoListEl.appendChild(todoItemEl);
-        todoItemEl.appendChild(changeTextOk);
-        
+    // 수정버튼 
+    const updateBtn = document.createElement("button");
+    updateBtn.classList.add("updateBtn");
+    updateBtn.innerHTML = `<span class="material-symbols-outlined">
+      edit</span>`;
+    updateBtn.addEventListener("click", (e) => {
+      changeText.classList.remove("off");
+      changeTextOk.classList.remove("off");
     })
+
+    const changeText = document.createElement("input");
+    changeText.classList.add("change-text");
+    changeText.classList.add("off");
+
+    // 확인버튼 누르면 다시 off 
+    const changeTextOk = document.createElement("button");
+    changeTextOk.classList.add("change-text_ok");
+    changeTextOk.classList.add("off");
+    changeTextOk.innerHTML= "확인"
+    changeTextOk.addEventListener("click", (e) => {
+      if(todoEl.value === changeText.value || changeText.value === "") {
+        changeText.classList.add("off");
+        changeTextOk.classList.add("off");
+      }else {
+        todoEl.value = changeText.value;
+        changeText.classList.add("off");
+        changeTextOk.classList.add("off");
+      }
+    })
+
+
+    todoItemEl.appendChild(checkboxEl);
+    todoItemEl.appendChild(todoEl);
+    todoItemEl.appendChild(deleteBtn);
+    todoItemEl.appendChild(updateBtn);
+    todoItemEl.appendChild(changeText);
+    todoListEl.appendChild(todoItemEl);
+    todoItemEl.appendChild(changeTextOk);
+        
+  })
 }
 
 const init = () => {
-    todoInputEl.addEventListener('keypress', (e) =>{ // Enter 키로 추가
-        if( e.key === 'Enter' && todoInputEl.value !== ''){ // 빈 값 입력 방지
-            appendTodos(e.target.value); 
-            todoInputEl.value ='';
-        }
-    })
+  // Enter 키로 추가
+  todoInputEl.addEventListener('keypress', (e) =>{ 
+    // 빈 값 입력 방지
+    if( e.key === 'Enter' && todoInputEl.value !== ''){ 
+      appendTodos(e.target.value); 
+      todoInputEl.value ='';
+    }
+  })
 
-    addBtn.addEventListener('click', () => { // 추가하기 버튼으로 추가
-      if(todoInputEl.value !== '') { // 빈 값 입력 방지
-        appendTodos(todoInputEl.value); 
-        todoInputEl.value ='';
-      }
+  // 추가하기 버튼으로 추가
+  addBtn.addEventListener('click', () => { 
+    // 빈 값 입력 방지
+    if(todoInputEl.value !== '') { 
+      appendTodos(todoInputEl.value); 
+      todoInputEl.value ='';
+    }
   })
 
 }
